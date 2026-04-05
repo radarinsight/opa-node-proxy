a608a022afa00b2fce44039c5ab5cd3836fafe68
radarinsight <edunevestrichez@gmail.com>
2026-04-05 14:18:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const axios = require('axios');
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 const OPA_BASE_URL = process.env.OPA_BASE_URL;
 const OPA_TOKEN = process.env.OPA_TOKEN;
